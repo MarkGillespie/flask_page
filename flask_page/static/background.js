82be5b23f7d77d1fd23a19bd81e34275dc5280bb
Mark Gillespie <mgillesp@caltech.edu>
2017-06-30 00:11:11 -0700

    Modified CSS
    Added styling to login page and website adding page. Changed background
    script to fade in background when it has loaded.

--- flask_page/static/background.js
+++ flask_page/static/background.js
@@ -1,5 +1,11 @@
-function getBackground(){
+function getBackground() {
     var background_data = g_backgrounds[Math.floor(Math.random() * g_backgrounds.length)];
-    var background = "url(\"" +background_data["url"] +"\")";
-    document.body.style.backgroundImage = background;
+    var background = "url(\"" + background_data["url"] +"\")";
+
+    var image = new Image();
+    image.onload = function () {
+        document.body.style.backgroundImage = "url(\"" +background_data["url"] +"\")";
+        document.getElementById("background").className += ' fadeOut';
+    }
+    image.src = background_data["url"];    
 }
